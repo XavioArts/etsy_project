acb580a664c1caabc394267e22be9b2c47bd0712
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-06 12:56:08 -0700

    get products axios call

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Button } from "antd";
 
 const Products = () => {
+
+    const [products, setProducts] = useState(null);
+
+    const getProducts = async () => {
+        try {
+            let res = await axios.get("/api/products");
+            setProducts(res.data);
+        } catch (err) {
+            console.log(err.response);
+            alert("there was an error getting products");
+        }
+    }
+
     return (
         <div>
-            <h1>Products</h1>
+            <h1>Products Page</h1>
+            <Button onClick={()=>getProducts()} >Get Products</Button>
+            {products && 
+                <div>
+                    <p>{JSON.stringify(products)}</p>
+                </div>}
         </div>
     );
 };
