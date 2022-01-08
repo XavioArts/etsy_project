d8927949fb32f62172a761977590af83c184358b
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-01-08 10:06:52 -0700

    findProduct axios call working

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -13,7 +13,7 @@ class Seller < ApplicationRecord
 
     def self.products_by_buyers
         select('sellers.id, sellers.email, sellers.name, b.id AS buyer_id, b.name AS buyer_name, b.max_price, 
-         b.desired_cat, p.id AS product_id, p.price, p.description, p.category')
+         b.desired_cat, b.seller_id, p.id AS product_id, p.price, p.description, p.category')
         .joins('JOIN buyers AS b 
         ON b.seller_id = sellers.id 
         JOIN products AS p 
--- client/src/pages/FindProduct.js
+++ client/src/pages/FindProduct.js
@@ -1,12 +1,75 @@
+import { Spin } from "antd";
 import Title from "antd/lib/typography/Title";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { PageDiv } from "../components/Styles";
 
 const FindProduct = () => {
 
+    const [data, setData] = useState(null);
+    const [sellers, setSellers] = useState(null);
+    const [buyers, setBuyers] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    const getData = async () => {
+        try {
+            let res = await axios.get("/api/find_products");
+            let normalized = normalizeData(res.data);
+            setData(normalized);
+            setLoading(false);
+        } catch (err) {
+            console.log(err.response);
+            alert("there was an error getting data")
+        }
+    };
+
+    useEffect(()=>{
+        getData();
+    }, []);
+
+    const normalizeData = (data) => {
+        let seller_ids = data.map((i) => i.id);
+        let sellersUnique = [...new Set(seller_ids)];
+        let newData = sellersUnique.map((id) => {
+            let buyers = data.filter((i) => i.seller_id === id);
+            let buyersIds = buyers.map((i)=>i.buyer_id);
+            let buyersUnique = [...new Set(buyersIds)];
+            let buyerData = buyersUnique.map((id) => {
+                let buyerProducts = data.filter((i)=> i.buyer_id === id)
+                let cleanProducts = buyerProducts.map((p) => {
+                    return {id: p.product_id, price: p.price, category: p.category, description: p.description};
+                })
+                return {buyer_id: buyerProducts[0].buyer_id, buyer_name: buyerProducts[0].buyer_name, 
+                    max_price: buyerProducts[0].max_price, desired_cat: buyerProducts[0].desired_cat, 
+                    products: cleanProducts};
+            })
+            return {id: buyers[0].id, name: buyers[0].name, email: buyers[0].email, buyers: buyerData};
+            // let cleanProducts = products.map((p) => {
+            //     return {id: p.id, price: p.price, description: p.description, category: p.category}
+            // });
+            // return {name: products[0].name, email: products[0].email, id: products[0].id, products: cleanProducts};
+        });
+        let sellerNames = newData.map((s)=>s.name)
+        // may need to add seller id here ^^^^
+        setSellers(sellerNames);
+        return newData;
+    }
+
+    if (loading) {
+        return (
+            <div style={{margin: "auto", padding: "100px", textAlign: "center"}} >
+                <Spin size="large" />
+            </div>
+        )
+    }
+
     return (
         <PageDiv>
             <Title>Find Product Page</Title>
+            <code>{JSON.stringify(sellers)}</code>
+            <hr/>
+            <code>{JSON.stringify(data)}</code>
         </PageDiv>
     );
 };
